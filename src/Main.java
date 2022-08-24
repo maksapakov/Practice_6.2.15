08aac65f248f31f0b68907bdeb27a89e5b51def2
Макс Апаков <maksapakov@gmail.com>
2022-08-24 10:33:54 +0300

    ver.1.0

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,21 @@
 import java.util.ArrayDeque;
 import java.util.Deque;
 import java.util.Scanner;
+
 // 1 2 3 4 5 6 7 8 9 10
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Deque<Integer> integerDeque = new ArrayDeque<>();
-        int i = 0;
-        while (scanner.hasNext()) {
-            if (i%2 == 0){
+        while (scanner.hasNextInt()) {
+            scanner.next();
+            if (scanner.hasNextInt()) {
                 integerDeque.addFirst(scanner.nextInt());
             }
-            i++;
         }
-        int lastNum = integerDeque.pollLast();
-        System.out.println(integerDeque);
-        System.out.println(lastNum);
+        for (int i :
+                integerDeque) {
+            System.out.printf("%d ", i);
+        }
     }
 }
